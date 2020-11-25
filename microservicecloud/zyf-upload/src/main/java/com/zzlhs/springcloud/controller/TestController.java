86872ab83972fc9b2d5349660fd6dfeb30181aaa
zyf <yuanfeng.zheng@yeahmobi.com>
2020-11-26 00:18:56 +0800

    123

--- microservicecloud/zyf-upload/src/main/java/com/zzlhs/springcloud/controller/TestController.java
+++ microservicecloud/zyf-upload/src/main/java/com/zzlhs/springcloud/controller/TestController.java
@@ -29,7 +29,7 @@ public class TestController {
 	
 	@RequestMapping(value = "/html", method = RequestMethod.GET)
 	public String test() {
-		System.out.println("123456789");
+		System.out.println("123456789012");
 		return "test";
 	}
 	
